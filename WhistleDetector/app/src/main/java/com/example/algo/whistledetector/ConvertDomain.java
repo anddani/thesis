08357de441f50c0c2d8b245ea6312f91612b364a
anddani <anddani@kth.se>
2017-02-27 07:41:27 +0100

    Added windowing and refactoring of code

---
+++ WhistleDetector/app/src/main/java/com/example/algo/whistledetector/ConvertDomain.java
@@ -0,0 +1,28 @@
+package com.example.algo.whistledetector;
+
+public class ConvertDomain {
+
+    public static void hanningWindow(double[] samples) {
+        for (int i = 1; i < samples.length; i++) {
+            samples[i] *= 0.5 * (1 - Math.cos((2 * Math.PI * i) / (samples.length-1)));
+        }
+    }
+
+    public static Complex[] timeToFrequency(short[] buffer, FFTColumbiaIterative fftci) {
+        int bufferSize = buffer.length;
+        double[] doubleBuffer = new double[SignalDetector.nextPowerOfTwo(bufferSize)];
+
+        // Convert to double
+        for (int i = 0; i < buffer.length; i++) {
+            doubleBuffer[i] = (double) buffer[i] / 32768.0;
+        }
+
+        // Apply windowing function
+        hanningWindow(doubleBuffer);
+
+        // Run FFT and get sample in frequency domain
+        Complex[] freqDomain = SignalDetector.getFrequencyDomain(doubleBuffer, fftci);
+
+        return freqDomain;
+    }
+}
--- WhistleDetector/app/src/main/java/com/example/algo/whistledetector/MainActivity.java
+++ WhistleDetector/app/src/main/java/com/example/algo/whistledetector/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.algo.whistledetector;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Handler;
-import android.os.Looper;
 import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -15,19 +17,35 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int SAMPLING_RATE = 44100;
+    public static final int TARGET_FREQUENCY = 1000;
+    public static final int MIN_MAG = 10;
+
     RelativeLayout rl;
     TextView tv_freq, tv_mag;
+
+    AudioRecord recorder;
+    int minBufferSize;
+    int bufferSize;
+    short[] buffer;
+
+    int counter = 0;
+
     private final Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             if (msg.what == 1) {
                 rl.setBackgroundColor(Color.GREEN);
-
-            } else if (msg.what == 2){
+                tv_freq.setText("Frequency: " + msg.arg1);
+                tv_mag.setText("Magnitude: " + msg.arg2);
+            } else if (msg.what == 2) {
                 rl.setBackgroundColor(Color.RED);
+                tv_freq.setText("Frequency: " + msg.arg1);
+                tv_mag.setText("Magnitude: " + msg.arg2);
+            } else if (msg.what == 3) {
+                System.out.println("Updating tv_freq");
+                tv_freq.setText("" + counter);
             }
-            tv_freq.setText("Frequency: " + msg.arg1);
-            tv_mag.setText("Magnitude: " + msg.arg2);
         }
     };
 
@@ -46,7 +64,50 @@ public class MainActivity extends AppCompatActivity {
 
         requestRecordAudioPermission();
 
-        new ReadMic(mHandler);
+        bufferSize = AudioRecord.getMinBufferSize(SAMPLING_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        System.out.println("Buffer size before " + bufferSize);
+        bufferSize = SignalDetector.nextPowerOfTwo(bufferSize);
+        System.out.println("Buffer size after " + bufferSize);
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLING_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
+        buffer = new short[bufferSize];
+
+        recorder.startRecording();
+
+        new SampleFromMic().start();
+    }
+
+    class SampleFromMic extends Thread {
+        @Override
+        public void run() {
+            FFTColumbiaIterative fftci = new FFTColumbiaIterative(bufferSize);
+
+            while (true) {
+                System.out.println("Min buffer size: " + bufferSize);
+
+                recorder.read(buffer, 0, buffer.length);
+                Complex[] freqDomain = ConvertDomain.timeToFrequency(buffer, fftci);
+
+                // Get frequency with max amplitude
+                double maxAmplitude = 0.0;
+                double bestFrequency = 0.0;
+                double frequencyFactor = (1.0 * ReadMic.SAMPLING_RATE) / (1.0 * bufferSize);
+                for (int i = 0; i < freqDomain.length/2; i++) {
+                    double frequency = i * frequencyFactor;
+                    double amplitude = freqDomain[i].abs();
+                    if (amplitude > maxAmplitude) {
+                        maxAmplitude = amplitude;
+                        bestFrequency = frequency;
+                    }
+                }
+
+                bestFrequency = ((int)maxAmplitude == 0) ? 0.0 : bestFrequency;
+                if (bestFrequency > TARGET_FREQUENCY - 100 && bestFrequency < TARGET_FREQUENCY + 100 && maxAmplitude > MIN_MAG) {
+                    mHandler.obtainMessage(1, (int)bestFrequency, (int)maxAmplitude).sendToTarget();
+                } else {
+                    mHandler.obtainMessage(2, (int)bestFrequency, (int)maxAmplitude).sendToTarget();
+                }
+            }
+        }
     }
 
     private void requestRecordAudioPermission() {
