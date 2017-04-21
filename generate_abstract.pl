cc21ea20119eef33cc84bf16b33f0407c2d76a21
anddani <anddani@kth.se>
2017-04-21 12:55:51 +0200

    Perl script for creating html abstract and sammanfattning

---
+++ generate_abstract.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/env perl
+
+# use strict;
+use warnings;
+
+my $author;
+my $title;
+my $swetitle;
+my $abstract;
+my $sammanfattning;
+
+while (<>) {
+    if (m/\\def\\thesistitle\{\\.*? (.*)\}/) {
+        $_ = $1;
+        s/\\//g; # Remove newline
+        $title = $_;
+    } elsif (m/\\def\\swethesistitle\{\\.*? (.*)\}/) {
+        $_ = $1;
+        s/\\//g; # Remove newline
+        $title = $_;
+    } elsif (m/\\def\\theauthor\{\\.*? (.*)\}/) {
+        $author = $1;
+    } elsif (m/\\def\\theabstract\{\\input\{(.*)\}\}/) {
+        open my $file, '<', $1; 
+        $abstract = <$file>; 
+        close $file;
+        $abstract =~ s/\\texttt\{(.*?)\}/<b>$1<\/b>/g;
+    } elsif (m/\\def\\thesweabstract\{\\input\{(.*)\}\}/) {
+        open my $file, '<', $1; 
+        $sammanfattning = <$file>; 
+        close $file;
+        $sammanfattning =~ s/\\texttt\{(.*?)\}/<b>$1<\/b>/g;
+    }
+}
+
+open (my $eng_abstract, '>', "abstract_eng.html") || die $!;
+my $eng_html = "<html>
+  <head>
+    <meta charset=\"UTF-8\" />
+    <title>$title</title>
+  </head>
+  <body>
+    <h1>$author</h1>
+    <h2>$title</h2>
+    <h3>Abstract</h3>
+    <p>$abstract</p>
+  </body>
+</html>
+";
+print $eng_abstract $eng_html;
+close $eng_abstract;
+
+open (my $swe_abstract, '>', "abstract_swe.html") || die $!;
+my $swe_html = "<html>
+  <head>
+    <meta charset=\"UTF-8\" />
+    <title>$swetitle</title>
+  </head>
+  <body>
+    <h1>$author</h1>
+    <h2>$swetitle</h2>
+    <h3>Sammanfattning</h3>
+    <p>$sammanfattning</p>
+  </body>
+</html>
+";
+print $swe_abstract $swe_html;
+close $swe_abstract;
